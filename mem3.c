99ba1afc50f1effd33f98a73a161aa6d1f307f80
fawadfwd <74730157+fawadfwd@users.noreply.github.com>
2022-03-19 18:28:27 +0500

    Add files via upload

--- mem3.c
+++ mem3.c
@@ -308,6 +308,8 @@ static ssize_t mychardev_read(struct file *file, char __user *buf, size_t count,
 	}	
 //	struct rtl8169_private *tp = dev_get_drvdata(&p->dev);
 	struct pci_slot *s=p->slot;
+	int slot = PCI_SLOT(p->devfn);
+	printk("WOWOWOWOWOWOW &&&&&&& slot = <%d>\n",slot);
 	if(s==NULL)
 	{
 		printk("************** slot NULL\n");
